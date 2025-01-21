246a742fa34eb572bd1cea0a9466da93dad647ce
Sadaf Rahman <62661608+rahmansadaf46@users.noreply.github.com>
2025-01-21 16:38:29 +0600

    meta data update

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,6 @@
+export const metadata = {
+    title: 'About Page',
+}
 export default function About() {
     return <h1>About Me</h1>
 }
---
+++ src/app/products/[productId]/layout.tsx
@@ -0,0 +1,12 @@
+export default function ProductsDetailsLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+        {children}
+        <p>Feature products</p>
+    </>
+  )
+}
