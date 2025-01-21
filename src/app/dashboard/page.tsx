9b42fad6de0aad0253ce96b268530ec231dd9412
Sadaf Rahman <62661608+rahmansadaf46@users.noreply.github.com>
2025-01-21 17:23:07 +0600

    metadata update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: {
+        absolute:'Dashboard'
+    },
+}
 export default function Home() {
     return <h1>welcome dashboard.!</h1>
 }
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,4 +1,23 @@
-export default async function ProductDetails({ params }: { params: Promise<{ productId: string }> }) {
+import { Metadata } from "next"
+
+type Props = {
+    params: {
+        productId: string;
+    }
+}
+export const generateMetadata = async({ params }: Props): Promise<Metadata> => {
+    const title = await new Promise((resolve) => { 
+        setTimeout(() => {
+            resolve(`iphone ${params.productId}`)
+        },100)
+    })
+    return {
+        title: `Product ${title}`
+    }
+}
+
+
+export default async function ProductDetails({ params }: Props) {
     const productId = (await params).productId
     return <h1>Product Details {productId} </h1>
 }
